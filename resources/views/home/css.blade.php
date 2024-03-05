cf64ea77b3647786542db61f5f5cbec149314335
yogakrishanthi98 <yogakrishanthi@konnect.team>
2024-03-05 17:01:49 +0530

    title changed

--- resources/views/home/css.blade.php
+++ resources/views/home/css.blade.php
@@ -8,7 +8,7 @@
 <meta name="description" content="" />
 <meta name="author" content="" />
 <link rel="shortcut icon" href="images/favicon.png" type="">
-<title>Famms - Fashion HTML Template</title>
+<title>Booker Wholesale UK | Foodservice | Cash & Carry | Booker.co.uk</title>
 <!-- bootstrap core css -->
 <link rel="stylesheet" type="text/css" href="home/css/bootstrap.css" />
 <!-- font awesome style -->
